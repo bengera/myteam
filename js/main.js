8822804b72086ba9ec12a0f872a3062b06f225b2
Ben <bengeraghty.bg@gmail.com>
2023-06-06 21:23:52 +0700

    add mobile nav

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 const hamMenu = document.querySelector('.hamburger-menu');
+const overlay = document.querySelector('.overlay');
+const navPrimary = document.querySelector('.nav-primary');
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
+    overlay.classList.toggle('show');
+    navPrimary.classList.toggle('show');
 })
