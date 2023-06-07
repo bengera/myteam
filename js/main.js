b280b4b1f49233b541425a1e56b7a93e551702f3
Ben <bengeraghty.bg@gmail.com>
2023-06-07 16:39:43 +0700

    fixed mob nav

--- js/main.js
+++ js/main.js
@@ -6,4 +6,13 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     overlay.classList.toggle('show');
     navPrimary.classList.toggle('show');
+
+    if (hamMenu.classList.contains('active')) {
+        console.log('menu-open');
+        document.body.setAttribute("style", "position:fixed");
+
+    } else {
+        overlay.setAttribute("style", "display:none");
+        document.body.setAttribute("style", "position:static");
+    }
 })
