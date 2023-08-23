8b7cf418f59e67d4ae4c073c5bc40b7c4d894a1a
Ben <bengeraghty.bg@gmail.com>
2023-08-23 22:20:46 +0700

    fix navigation scroll issue

--- js/main.js
+++ js/main.js
@@ -6,14 +6,16 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     overlay.classList.toggle('show');
     navPrimary.classList.toggle('show');
+    
 
     if (hamMenu.classList.contains('active')) {
         console.log('menu-open');
-        document.body.setAttribute("style", "position:fixed");
-
+        // document.body.setAttribute("style", "position:fixed");
+        document.body.style.overflowY = 'hidden';
     } else {
         overlay.classList.toggle('active');
-        document.body.setAttribute("style", "position:static");
+        // document.body.setAttribute("style", "position:static");
+        document.body.style.overflowY = 'visible';
     }
 });
 
