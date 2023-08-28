ee0ac3be428936df7081cf87aec0f778079ab3dd
Ben <bengeraghty.bg@gmail.com>
2023-08-28 20:29:56 +0700

    add mobile menu reset function

--- js/main.js
+++ js/main.js
@@ -10,12 +10,36 @@ hamMenu.addEventListener('click', () => {
 
     if (hamMenu.classList.contains('active')) {
         console.log('menu-open');
-        // document.body.setAttribute("style", "position:fixed");
         document.body.style.overflowY = 'hidden';
     } else {
         overlay.classList.toggle('active');
-        // document.body.setAttribute("style", "position:static");
         document.body.style.overflowY = 'visible';
     }
 });
 
+function resetMenu() {
+    console.log('reset');
+    document.body.setAttribute("style", "position:static");
+    overlay.setAttribute("style", "display:none");
+    hamMenu.classList.toggle('active');
+    navPrimary.classList.remove('show');
+
+
+}
+
+(function () {
+    window.onresize = displayWindowSize;
+    window.onload = displayWindowSize;
+
+    function displayWindowSize() {
+        let myWidth = window.innerWidth;
+
+        if (myWidth >= 600) {
+            if (hamMenu.classList.contains('active')) {
+                resetMenu();
+            }
+            
+        } 
+
+    }
+})();
