1b33353c9974026ac99b92bf40ed8efe35ced304
Ben <bengeraghty.bg@gmail.com>
2023-08-29 15:45:16 +0700

    add initial form feature functions

---
+++ js/form.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('form');
+const fullName = document.getElementById('fullname');
+const email = document.getElementById('email');
+const companyName = document.getElementById('company');
+const title = document.getElementById('title');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('submitted');
+    checkInputs();
+})
+
+function checkInputs() {
+    const fullNameValue = fullName.value.trim();
+    const emailValue = email.value.trim();
+    const companyValue = company.value.trim();
+    const titleValue = title.value.trim();
+    
+    if (fullNameValue === '') {
+        setErrorFor(fullName, 'This field is required');
+    } else {
+        setSuccessFor(fullName);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+    formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = '';
+    formControl.className = 'form-control success';
+}
