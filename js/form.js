a462e01ac07be4a827397e852b7c93ff9c72c25e
Ben <bengeraghty.bg@gmail.com>
2023-08-29 16:10:55 +0700

    complete form check functionality

--- js/form.js
+++ js/form.js
@@ -16,11 +16,26 @@ function checkInputs() {
     const emailValue = email.value.trim();
     const companyValue = company.value.trim();
     const titleValue = title.value.trim();
+    const messageValue = message.value.trim();
     
     if (fullNameValue === '') {
         setErrorFor(fullName, 'This field is required');
     } else {
         setSuccessFor(fullName);
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Please enter an email address');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Please use a valid email address');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (messageValue === '') {
+        setErrorFor(message, 'This field is required');
+    } else {
+        setSuccessFor(message);
     }
 }
 
@@ -36,4 +51,9 @@ function setSuccessFor(input) {
     const small = formControl.querySelector('small');
     small.innerText = '';
     formControl.className = 'form-control success';
+}
+
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
